e93f16c2b557aad3826c4b408d88f7fa892fef26
Francesco <demonerosso77@gmail.com>
2018-11-16 23:37:59 +0100

    Nuovo repo Java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld // DICHIARAZIONE DELLA CLASSE HelloWorld. La classe è dichiarata publica con il modificatore public
+{  // INIZIO DELLA CLASSE HelloWorld
+	
+	public static void main(String[] args)  // QUESTA RIGA DEVE ESSERE DEFINITA IN OGNI APPLICAIZONE. E' IL PUNTO DI PARTENZA. METODO MAIN = AZIONE MAIN
+	{
+		System.out.println("Hello World!");
+	
+	}
+
+}
---
+++ NumeroIntero.java
@@ -0,0 +1,17 @@
+public class NumeroIntero {
+public int NumeroIntero;
+
+public NumeroIntero() {} // COSTRUTTORE
+
+public void NumeroIntero(){
+	System.out.println(NumeroIntero);
+}
+
+public static void main(String[] args)
+{
+	NumeroIntero number = new NumeroIntero();
+	number.NumeroIntero = 5;
+	number.NumeroIntero();
+	
+}
+}
